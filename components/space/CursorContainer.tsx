a1d2f66923902485edda9af387e5748ded730674
Isabella <idavila2021@gmail.com>
2024-08-14 13:24:56 +0100

    fix: if default cursor render <></>

--- components/space/CursorContainer.tsx
+++ components/space/CursorContainer.tsx
@@ -34,7 +34,6 @@ const renderCursors = (
 ) => {
 	return Object.keys(users).map((uuid) => {
 		const user = users[uuid];
-		const cursor = user.state?.cursor || "";
 		const color = user.state?.color || "";
 		const x = user.state?.x ?? 0;
 		const y = user.state?.y ?? 0;
--- components/space/CustomCursor.tsx
+++ components/space/CustomCursor.tsx
@@ -2,10 +2,9 @@
 
 import * as React from "react";
 import { usePerfectCursor } from "../hooks/useCursor";
-
 interface CursorProps {
 	point: [number, number];
-	imageUrl: string; // Update the prop to use an image URL
+	imageUrl: string;
 	username: string;
 }
 
@@ -37,12 +36,20 @@ export function CustomCursor({ point, imageUrl, username }: CursorProps) {
 			}}
 			className="flex flex-col items-center"
 		>
-			<img
-				src={imageUrl}
-				alt="Custom Cursor"
-				className="rounded-xl w-10 h-10"
-			/>
-			<p className="bg-white text-black mt-2 px-1 rounded-[10px] font-semibold">{username}</p>
+			{imageUrl === "/default.png" ? (
+				<></>
+			) : (
+				<>
+					<img
+						src={imageUrl}
+						alt="Custom Cursor"
+						className="rounded-xl w-10 h-10"
+					/>
+					<p className="bg-white text-black mt-2 px-1 rounded-[10px] font-semibold">
+						{username}
+					</p>
+				</>
+			)}
 		</div>
 	);
 }
