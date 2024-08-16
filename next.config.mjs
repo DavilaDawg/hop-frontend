05adcee5a670e30f6f87a1dccc846c847ba8922b
Isabella <idavila2021@gmail.com>
2024-08-16 22:50:13 +0100

    fix: nextConfig for cloudinary

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  images: {
+    remotePatterns: [{ protocol: "https", hostname: "res.cloudinary.com" }],
+  },
 };
 
 export default nextConfig;
