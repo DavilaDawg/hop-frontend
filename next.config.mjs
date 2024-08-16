bfcc1025fc8c62b5032b883524c92bfc189ac06b
Isabella <idavila2021@gmail.com>
2024-08-16 21:16:50 +0100

    fix(nextconfig): delete image domains

--- next.config.mjs
+++ next.config.mjs
@@ -3,31 +3,6 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "tse2.mm.bing.net",
-      },
-      {
-        protocol: "https",
-        hostname: "media.istockphoto.com",
-      },
-      {
-        protocol: "https",
-        hostname: "tse3.explicit.bing.net",
-      },{
-        protocol: "https",
-        hostname: "i.imgur.com",
-      },{
-        protocol: "https",
-        hostname: "imgur.com",
-      },
-      {protocol: "https",
-        hostname: "res.cloudinary.com"
-      }
-    ],
-  },
 };
 
 export default nextConfig;
