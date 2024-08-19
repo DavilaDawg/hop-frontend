67d51bacefc10a42ec5a3fbe74b9aa3b06830dbc
Isabella <idavila2021@gmail.com>
2024-08-19 22:56:55 +0100

    c

--- components/space/ChatContainer.tsx
+++ components/space/ChatContainer.tsx
@@ -119,7 +119,7 @@ const ChatContainer: React.FC = () => {
 		<div className="flex flex-col h-screen p-4">
 			<p className="text-3xl font-semibold p-2">Chat</p>
 
-			<div className="bg-white rounded-xl p-4 flex flex-col max-h-screen message-container">
+			<div className="bg-white rounded-xl p-4 flex flex-col max-h-[320px] message-container">
 				<div className="flex flex-col message-list overflow-y-scroll">
 					{messages.map((msg, index) => {
 						if (!msg.username) return null;
